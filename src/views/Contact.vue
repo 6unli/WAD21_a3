4c2be50c2a1354c8259259c19f2bfa638f40cff3
maiko29 <36730047+maiko29@users.noreply.github.com>
2021-11-21 21:41:42 +0200

    Testing different approach

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,43 +4,36 @@
     <br />
     <p>You are welcome to contact us on Workdays from Dusk till Dawn.</p>
     <p>You will find us in a dark and empty place.</p>
-    <p>Online You can find us on the dark web.</p>
-    <br/>
-    <table>
-      <tr>
-        <td>&#128222;:</td>
-        <td>+372 5345 6789</td>
-      </tr>
-      <tr>
-        <td>&#128231;:</td>
-        <td>homework3@wad.com</td>
-      </tr>
-      <tr>
-        <td>&#127970;:</td>
-        <td>Narva mnt 18, 51009 Tartu</td>
-      </tr>
-      <tr>
-        <td>&#127760;:</td>
-        <td>github.com/6unli/WAD21_a3</td>
-      </tr>
-    </table>
+    <p>
+      Online You can find us on the dark web... or just contact with us with the following channels:    </p>
+    <div v-for="record in contacts" :key="record.phone" :email="record.email">
+      <p><br>Tel: {{ record.phone }}<br> e-mail: {{ record.email }}</p>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  name: "contact",
+  data() {
+    return {
+      contacts: [
+        { email: "wad2021@wad2021.ee", phone: "+372123456" },
+        { email: "wad1900@wad1900.ee", phone: "+372654321" },
+      ],
+    };
+  },
+};
+</script>
+
+
+<style>
 .contact {
   margin: auto;
   background-color: whitesmoke;
   width: 45%;
-  min-height: calc(100vh - 372px);
-
+  height: 50vh;
   padding: 104px 80px;
   align-items: center;
-  line-height: 130%;
-}
-
-td {
-  padding-top: 8px;
-  padding-right: 8px;
 }
 </style>
