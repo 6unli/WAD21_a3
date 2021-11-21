7d4d5d7fdc92f805e9f848e4446fe8ae0109bc78
maiko29 <36730047+maiko29@users.noreply.github.com>
2021-11-21 17:02:03 +0200

    Contact view first commit

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="contact">
-    <h2>Contact page</h2>
-  </div>
+<div class="contact">    
+      <h2>Contact</h2><br>
+      <p>You are welcome to contact us on Workdays from Dusk till Dawn.</p>
+      <p>You will find us in a dark and empty place.</p>
+      <p>Online You can find us on the dark web.</p>
+    </div>
 </template>
+
+<style>
+.contact {
+  margin: auto;
+  background-color: whitesmoke;
+  width: 45%;
+  height: 70vh;
+
+  padding: 104px 80px;
+  align-items: center;
+}
+</style>
